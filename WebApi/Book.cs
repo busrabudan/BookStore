575c254fd41ad2311e90e9f6324c2c0cadbc51ad
busrabudan <busrabudan@outlook.com>
2023-02-22 16:00:21 +0300

    Update

--- WebApi/Book.cs
+++ WebApi/Book.cs
@@ -1,7 +1,7 @@
 namespace WebApi{
     public class Book{
         public int Id { get; set; }
-        public String Title { get; set; }
+        public string Title { get; set; }
         public int GenreId { get; set; }
         public int PageCount { get; set; }
         public DateTime PublisDate { get; set; }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,8 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApi.DBOperations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookStoreDB")); 
 
 var app = builder.Build();
+
+using(var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
